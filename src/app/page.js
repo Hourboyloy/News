a714693b1bce982da8d0fc8625eb1a2ab5706936
Hourboyloy <chhoysienghour@gmail.com>
2024-09-26 12:38:28 +0700

    update

--- src/app/page.js
+++ src/app/page.js
@@ -24,7 +24,7 @@ export default function Home() {
         }
       })
       .catch((err) => {
-        setError(err.message);
+        console.log(err.message);
       });
 
     axios
@@ -36,7 +36,7 @@ export default function Home() {
         }
       })
       .catch((err) => {
-        setError(err.message);
+        console.log(err.message);
       });
   }, []);
 
