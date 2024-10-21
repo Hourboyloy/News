fb014b10cf979edd5070c28042012b4c7db8cb60
Hourboyloy <chhoysienghour@gmail.com>
2024-10-21 12:02:23 +0700

    last commit

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -687,7 +687,7 @@ function Cards(props) {
   const [loading, setLoading] = useState(false);
   const [hasMoreNews, setHasMoreNews] = useState(true);
   const [CardsDelays, setCardDelays] = useState({});
-  const delaytime = newsRender.length <= 0 ? 0 : 500;
+  const delaytime = newsRender.length <= 0 ? 0 : 0;
 
   // Initialize AOS for animations
   useEffect(() => {
