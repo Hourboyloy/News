a824e8290023726c03a238e407bc6fb5a9b51d55
Hourboyloy <chhoysienghour@gmail.com>
2024-10-24 14:21:53 +0700

    last cm

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,6 +5,7 @@ import "aos/dist/aos.css";
 import LoadingCardsComponent from "./LoadingCards";
 import { useNewsWeb } from "@/context/NewsContext";
 
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 function Cards(props) {
   const { LoadingCard } = useNewsWeb();
   const { news, loadMoreNews, LoadingCardByPage } = props;
@@ -268,13 +269,104 @@ function Cards(props) {
               </li>
             ))}
         </ul>
-        {LoadingCard && news.length <= 0 ? (
-          <LoadingCardsComponent />
-        ) : LoadingCardByPage ? (
-          <LoadingCardsComponent />
-        ) : (
-          ""
-        )}
+        {LoadingCard && news.length <= 0
+          ? cards.length > 0 &&
+            cards.map((ele, i) => (
+              <li
+                data-aos="fade-up"
+                data-aos-delay={100}
+                className="w-full h-full rounded-lg"
+                key={i + ele}
+              >
+                <a
+                  href={ele.articleUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="w-full h-full rounded-lg group outline-none focus:outline-none"
+                >
+                  <div className="isHoverStyle cursor-pointer bg-gray-800 h-full overflow-hidden w-full border-gray-900 rounded-lg shadow-gray-600 transition-all duration-500 ease-in-out">
+                    <div>
+                      <div className="lg:h-48 md:h-52 h-56 relative">
+                        {/* Fallback background pulse animation */}
+                        <div className="h-full w-full absolute z-20 top-0 left-0 transition-opacity duration-300 ease-in-out opacity-100 bg-gray-600 animate-pulse"></div>
+                      </div>
+                    </div>
+
+                    <div className="p-3 space-y-3 transition-all duration-500 ease-in-out">
+                      <div className="cursor-pointer transition-opacity duration-500 ease-in-out">
+                        <p className="bg-gray-600 py-[5px] w-24 rounded animate-pulse"></p>
+                      </div>
+
+                      <div className="transition-opacity duration-500 ease-in-out">
+                        <div className="space-y-2.5">
+                          <p className="bg-gray-600 py-[5px] w-11/12 rounded animate-pulse"></p>
+                          <p className="bg-gray-600 py-[5px] w-8/12 rounded animate-pulse"></p>
+                        </div>
+                      </div>
+
+                      <div className="text-gray-400 flex justify-between items-center">
+                        <div>
+                          <p className="bg-gray-600 py-[5px] w-16 rounded animate-pulse"></p>
+                        </div>
+
+                        <div>
+                          <p className="bg-gray-600 py-[5px] w-12 rounded animate-pulse"></p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </li>
+            ))
+          : LoadingCardByPage
+          ? cards.map((ele, i) => (
+              <li
+                data-aos="fade-up"
+                data-aos-delay={100}
+                className="w-full h-full rounded-lg"
+                key={i + ele}
+              >
+                <a
+                  href={ele.articleUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="w-full h-full rounded-lg group outline-none focus:outline-none"
+                >
+                  <div className="isHoverStyle cursor-pointer bg-gray-800 h-full overflow-hidden w-full border-gray-900 rounded-lg shadow-gray-600 transition-all duration-500 ease-in-out">
+                    <div>
+                      <div className="lg:h-48 md:h-52 h-56 relative">
+                        {/* Fallback background pulse animation */}
+                        <div className="h-full w-full absolute z-20 top-0 left-0 transition-opacity duration-300 ease-in-out opacity-100 bg-gray-600 animate-pulse"></div>
+                      </div>
+                    </div>
+
+                    <div className="p-3 space-y-3 transition-all duration-500 ease-in-out">
+                      <div className="cursor-pointer transition-opacity duration-500 ease-in-out">
+                        <p className="bg-gray-600 py-[5px] w-24 rounded animate-pulse"></p>
+                      </div>
+
+                      <div className="transition-opacity duration-500 ease-in-out">
+                        <div className="space-y-2.5">
+                          <p className="bg-gray-600 py-[5px] w-11/12 rounded animate-pulse"></p>
+                          <p className="bg-gray-600 py-[5px] w-8/12 rounded animate-pulse"></p>
+                        </div>
+                      </div>
+
+                      <div className="text-gray-400 flex justify-between items-center">
+                        <div>
+                          <p className="bg-gray-600 py-[5px] w-16 rounded animate-pulse"></p>
+                        </div>
+
+                        <div>
+                          <p className="bg-gray-600 py-[5px] w-12 rounded animate-pulse"></p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </li>
+            ))
+          : ""}
       </div>
     </div>
   );
--- src/components/LoadingCards.js
+++ src/components/LoadingCards.js
@@ -1,67 +1,59 @@
-import React from 'react'
+import React from "react";
 
-const cards = [1,2,3,4,5,6,7,8,9,10];
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
 function LoadingCardsComponent() {
   return (
-    <div>
-      <ul className="grid gap-3 2xl:grid-cols-5 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 grid-cols-1 pb-5">
-        {cards.length > 0 &&
-          cards.map((ele, i) => (
-            <li
-              data-aos="fade-up"
-              data-aos-delay={100}
-              className="w-full h-full rounded-lg"
-              key={i + ele}
+    <ul>
+      {cards.length > 0 &&
+        cards.map((ele, i) => (
+          <li
+            data-aos="fade-up"
+            data-aos-delay={100}
+            className="w-full h-full rounded-lg"
+            key={i + ele}
+          >
+            <a
+              href={ele.articleUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-full h-full rounded-lg group outline-none focus:outline-none"
             >
-              <a
-                href={ele.articleUrl}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="w-full h-full rounded-lg group outline-none focus:outline-none"
-              >
-                <div className="isHoverStyle cursor-pointer bg-gray-800 h-full overflow-hidden w-full border-gray-900 rounded-lg shadow-gray-600 transition-all duration-500 ease-in-out">
-                  <div>
-                    <div className="lg:h-48 md:h-52 h-56 relative">
-                      {/* Image Display */}
+              <div className="isHoverStyle cursor-pointer bg-gray-800 h-full overflow-hidden w-full border-gray-900 rounded-lg shadow-gray-600 transition-all duration-500 ease-in-out">
+                <div>
+                  <div className="lg:h-48 md:h-52 h-56 relative">
+                    {/* Fallback background pulse animation */}
+                    <div className="h-full w-full absolute z-20 top-0 left-0 transition-opacity duration-300 ease-in-out opacity-100 bg-gray-600 animate-pulse"></div>
+                  </div>
+                </div>
 
-                      {/* Fallback background pulse animation */}
-                      <div
-                        className={`h-full w-full absolute z-20 top-0 left-0 transition-opacity duration-300 ease-in-out opacity-100 bg-gray-600 animate-pulse
-                        `}
-                      ></div>
+                <div className="p-3 space-y-3 transition-all duration-500 ease-in-out">
+                  <div className="cursor-pointer transition-opacity duration-500 ease-in-out">
+                    <p className="bg-gray-600 py-[5px] w-24 rounded animate-pulse"></p>
+                  </div>
+
+                  <div className="transition-opacity duration-500 ease-in-out">
+                    <div className="space-y-2.5">
+                      <p className="bg-gray-600 py-[5px] w-11/12 rounded animate-pulse"></p>
+                      <p className="bg-gray-600 py-[5px] w-8/12 rounded animate-pulse"></p>
                     </div>
                   </div>
 
-                  <div
-                    className={`p-3 space-y-3 transition-all duration-500 ease-in-out`}
-                  >
-                    <div className="cursor-pointer transition-opacity duration-500 ease-in-out">
-                      <p className="bg-gray-600 py-[5px] w-24 rounded animate-pulse"></p>
+                  <div className="text-gray-400 flex justify-between items-center">
+                    <div>
+                      <p className="bg-gray-600 py-[5px] w-16 rounded animate-pulse"></p>
                     </div>
 
-                    <div className="transition-opacity duration-500 ease-in-out">
-                      <div className="space-y-2.5">
-                        <p className="bg-gray-600 py-[5px] w-11/12 rounded animate-pulse"></p>
-                        <p className="bg-gray-600 py-[5px] w-8/12 rounded animate-pulse"></p>
-                      </div>
-                    </div>
-
-                    <div className="text-gray-400 flex justify-between items-center">
-                      <div className="">
-                        <p className="bg-gray-600 py-[5px] w-16 rounded animate-pulse"></p>
-                      </div>
-
-                      <div className="">
-                        <p className="bg-gray-600 py-[5px] w-12 rounded animate-pulse"></p>
-                      </div>
+                    <div>
+                      <p className="bg-gray-600 py-[5px] w-12 rounded animate-pulse"></p>
                     </div>
                   </div>
                 </div>
-              </a>
-            </li>
-          ))}
-      </ul>
-    </div>
+              </div>
+            </a>
+          </li>
+        ))}
+    </ul>
   );
 }
 
